137d13f2fd5199ee92ddcd532d1e4dfa8c3b7e3c
firstquarterlee <firstquarterlee@gmail.com>
2023-10-15 19:02:34 +0900

    🎯 Collection for

--- main.dart
+++ main.dart
@@ -1,6 +1,10 @@
 void main() {
-  var name = 'nico';
-  var age = 10;
-  var greeting = "Hello everyone, my name is $name and I'm ${age + 2}";
-  print(greeting);
+  var oldFriends = ['nico', 'lynn'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    for (var friend in oldFriends) "❤️‍🔥$friend",
+  ];
+  print(newFriends);
 }
