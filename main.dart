81ed01fc54c9bd1cb389d8aa79dd1d02f6234be0
firstquarterlee <firstquarterlee@gmail.com>
2023-10-15 18:54:32 +0900

    🎯 String Interpolation

--- main.dart
+++ main.dart
@@ -1,15 +1,6 @@
-// void main() {
-//   var giveMeFive = true;
-//   var numbers = [1, 2, 3, 4, if (giveMeFive) 5];
-//   print(numbers);
-// }
-
 void main() {
-  var giveMeFive = true;
-  var numbers = [1, 2, 3, 4];
-  if (giveMeFive) {
-    numbers.add(5);
-  }
-
-  print(numbers);
+  var name = 'nico';
+  var age = 10;
+  var greeting = "Hello everyone, my name is $name and I'm ${age + 2}";
+  print(greeting);
 }
