0d096e0f648d231a4c8cf9b4846eb06e605489c7
firstquarterlee <firstquarterlee@gmail.com>
2023-10-16 23:30:15 +0900

    🎯Named Constructors

--- main.dart
+++ main.dart
@@ -1,6 +1,28 @@
 class Player {
-  final String name = 'nico';
-  int xp = 1500;
+  final String name;
+  int xp, age;
+  String team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
+
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
 
   void sayHello() {
     print("Hi my name is $name");
@@ -8,6 +30,9 @@ class Player {
 }
 
 void main() {
-  var player = Player();
-  player.sayHello();
+  var player = Player.createBluePlayer(
+    name: 'nico',
+    age: 21,
+  );
+  var redPlayer = Player.createRedPlayer("nico", 21);
 }
